a3a656ffa016aa8bc725d27d75c6f00d0a6e0351
Ayeda Francois DANIEL <100407568+ayeda98@users.noreply.github.com>
2024-11-27 18:14:59 +0100

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -10,7 +10,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey('Author', on_delete=models.CASCADE)
    
-class Library(models.Model):
+class library(models.Model):
     name =  models.CharField(max_length=200)
     books = models.ManyToManyField('Book')
 
