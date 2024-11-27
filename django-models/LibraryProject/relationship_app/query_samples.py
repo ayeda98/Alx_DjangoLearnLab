44d35ab9fda32ae472c16e29a08c40ce527e3b2d
Ayeda Francois DANIEL <100407568+ayeda98@users.noreply.github.com>
2024-11-27 18:16:05 +0100

    Update query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -5,7 +5,7 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
 django.setup()
 
-from relationship_app.models import Author, Book, Library, Librarian
+from relationship_app.models import Author, Book, library, Librarian
 
 def query_books_by_author(author_id):
     # Query all books by a specific author
@@ -21,13 +21,13 @@ def query_books_by_author(author_id):
 def list_all_books_in_library(library_id):
     # List all books in a library
     try:
-        library = Library.objects.get(id=library_id)
+        library = library.objects.get(id=library_id)
         books = library.books.all()  # Corrected to access the related books
         print(f"Books in the library {library.name}:")
         for book in books:
             print(f"- {book.title}")
     except Library.DoesNotExist:
-        print(f"Library with ID {library_id} does not exist.")
+        print(f"library with ID {library_id} does not exist.")
 
 def retrieve_librarian_for_library(library_id):
     # Retrieve the librarian for a library
@@ -36,7 +36,7 @@ def retrieve_librarian_for_library(library_id):
         librarian = library.librarian  # Corrected to access the related librarian
         print(f"The librarian for the library {library.name} is {librarian.name}.")
     except Library.DoesNotExist:
-        print(f"Library with ID {library_id} does not exist.")
+        print(f"library with ID {library_id} does not exist.")
 
 if __name__ == '__main__':
     # Example usage
