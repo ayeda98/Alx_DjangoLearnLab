806adc5c67b729906ac05593a0df657cdabbdf41
Ayeda <ayedafrancois1998@gmail.com>
2025-01-08 09:21:24 +0100

    update vue

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from .models import Book
+from django.views.generic import DetailView
+from .models import Book, Library
 
+# Vue basée sur les fonctions pour lister les livres
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
+# Vue basée sur les classes pour afficher les détails d'une bibliothèque
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
