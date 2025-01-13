d35db53951320790d72b5a7cc6a2523b3c15ed42
Ayeda <ayedafrancois1998@gmail.com>
2025-01-13 21:23:07 +0100

    second

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -27,3 +27,46 @@ def profile(request):
     else:
         form = ProfileForm(instance=request.user)
     return render(request, 'blog/profile.html', {'form': form})
+
+
+
+# Liste des articles
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+# Détail d'un article
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+# Créer un nouvel article
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+# Modifier un article
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def get_queryset(self):
+        return Post.objects.filter(author=self.request.user)
+
+# Supprimer un article
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post_list')
+
+    def get_queryset(self):
+        return Post.objects.filter(author=self.request.user)
+
