f7ee541b38d43658931e525565aac19f0c513482
Ayeda <ayedafrancois1998@gmail.com>
2025-01-13 21:10:51 +0100

    model post

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)  # Titre de l'article
+    content = models.TextField()  # Contenu de l'article
+    published_date = models.DateTimeField(auto_now_add=True)  # Date de publication (automatique)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')  # Auteur de l'article
+
+    def __str__(self):
+        return self.title
